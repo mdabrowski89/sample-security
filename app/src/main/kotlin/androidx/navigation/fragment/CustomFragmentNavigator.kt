e99c8d0d7578660086b09e3fb53e5011e86def51
mdabrowski89 <mobite.app@gmail.com>
2018-12-19 22:48:15 +0100

    WIP migration to Navigation controller

---
+++ app/src/main/kotlin/androidx/navigation/fragment/CustomFragmentNavigator.kt
@@ -0,0 +1,89 @@
+package androidx.navigation.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.IdRes
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+
+
+class CustomFragmentNavigator(
+    context: Context,
+    fragmentManager: FragmentManager,
+    private val containerId: Int
+): FragmentNavigator(context, fragmentManager, containerId) {
+
+    private val TAG = "CustomFragmentNavigator"
+
+    override fun navigate(
+        destination: Destination,
+        args: Bundle?,
+        navOptions: NavOptions?,
+        navigatorExtras: Navigator.Extras?
+    ) {
+        if (mFragmentManager.isStateSaved) {
+            Log.i(TAG, "Ignoring navigate() call: FragmentManager has already" + " saved its state")
+            return
+        }
+        val frag = destination.createFragment(args)
+        val ft = mFragmentManager.beginTransaction()
+
+        var enterAnim = navOptions?.enterAnim ?: -1
+        var exitAnim = navOptions?.exitAnim ?: -1
+        var popEnterAnim = navOptions?.popEnterAnim ?: -1
+        var popExitAnim = navOptions?.popExitAnim ?: -1
+        if (enterAnim != -1 || exitAnim != -1 || popEnterAnim != -1 || popExitAnim != -1) {
+            enterAnim = if (enterAnim != -1) enterAnim else 0
+            exitAnim = if (exitAnim != -1) exitAnim else 0
+            popEnterAnim = if (popEnterAnim != -1) popEnterAnim else 0
+            popExitAnim = if (popExitAnim != -1) popExitAnim else 0
+            ft.setCustomAnimations(enterAnim, exitAnim, popEnterAnim, popExitAnim)
+        }
+
+        ft.replace(containerId, frag)
+        ft.setPrimaryNavigationFragment(frag)
+
+        @IdRes val destId = destination.id
+        val initialNavigation = mBackStack.isEmpty()
+        val isClearTask = navOptions != null && navOptions.shouldClearTask()
+        // TODO Build first class singleTop behavior for fragments
+        val isSingleTopReplacement = (navOptions != null && !initialNavigation
+                && navOptions.shouldLaunchSingleTop()
+                && mBackStack.peekLast() == destId)
+
+        val backStackEffect: Int
+        if (initialNavigation || isClearTask) {
+            backStackEffect = Navigator.BACK_STACK_DESTINATION_ADDED
+        } else if (isSingleTopReplacement) {
+            // Single Top means we only want one instance on the back stack
+            if (mBackStack.size > 1) {
+                // If the Fragment to be replaced is on the FragmentManager's
+                // back stack, a simple replace() isn't enough so we
+                // remove it from the back stack and put our replacement
+                // on the back stack in its place
+                mFragmentManager.popBackStack()
+                ft.addToBackStack(Integer.toString(destId))
+                mIsPendingBackStackOperation = true
+            }
+            backStackEffect = Navigator.BACK_STACK_UNCHANGED
+        } else {
+            ft.addToBackStack(Integer.toString(destId))
+            mIsPendingBackStackOperation = true
+            backStackEffect = Navigator.BACK_STACK_DESTINATION_ADDED
+        }
+        if (navigatorExtras is Extras) {
+            for ((key, value) in navigatorExtras.sharedElements) {
+                ft.addSharedElement(key, value)
+            }
+        }
+        ft.setReorderingAllowed(true)
+        ft.commit()
+        // The commit succeeded, update our view of the world
+        if (backStackEffect == Navigator.BACK_STACK_DESTINATION_ADDED) {
+            mBackStack.add(destId)
+        }
+        dispatchOnNavigatorNavigated(destId, backStackEffect)
+    }
+}
--- app/src/main/kotlin/pl/mobite/sample/security/ui/components/MainActivity.kt
+++ app/src/main/kotlin/pl/mobite/sample/security/ui/components/MainActivity.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import com.google.android.material.navigation.NavigationView
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavigatorProvider
+import androidx.navigation.fragment.CustomFragmentNavigator
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_content.*
 import pl.mobite.sample.security.R
-import pl.mobite.sample.security.ui.components.fingerprint.FingerprintFragment
-import pl.mobite.sample.security.ui.components.pin.PinFragment
-import pl.mobite.sample.security.ui.components.secretkey.SecretKeyFragment
-import pl.mobite.sample.security.ui.custom.NavItem
 
 class MainActivity : AppCompatActivity() {
 
-    private var currentNavItem: NavItem? = null
+    /*private var currentNavItem: NavItem? = null
 
     private val navItems = listOf(
             NavItem(R.id.navItemSecretKey, "SECRET_KEY_FRAGMENT_TAG", R.string.secret_key_screen_title),
@@ -35,43 +37,53 @@ class MainActivity : AppCompatActivity() {
         navItems.find { it.navItemId == item.itemId }?.let { navItem -> showFragment(navItem) }
         drawerLayout.closeDrawer(GravityCompat.START)
         true
-    }
+    }*/
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        navController = findNavController(mainNavHostFragment)
+
         ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.main_nav_drawer_open_desc, R.string.main_nav_drawer_close_desc)
                 .apply {
                     drawerLayout.addDrawerListener(this)
                     syncState()
                 }
-
-        navigationView.setNavigationItemSelectedListener(navItemSelectedListener)
-        showFragment(savedInstanceState?.getParcelable(NavItem.PARCEL_KEY) ?: navItems.first())
+        //navigationView.setNavigationItemSelectedListener(navItemSelectedListener)
+        //showFragment(savedInstanceState?.getParcelable(NavItem.PARCEL_KEY) ?: navItems.first())
+       // navController.graph.findNode(R)
+        //val navigationProvider = NavigatorProvider()
+        navController.navigatorProvider.addNavigator("CustomFragmentNavigator", CustomFragmentNavigator(this, supportFragmentManager, R.id.mainNavHostFragment))
+        //val customGraph = NavGraph(CustomFragmentNavigator(this, supportFragmentManager, R.id.mainNavHostFragment))
+        NavigationUI.setupWithNavController(navigationView, navController)
     }
+    
+    override fun onSupportNavigateUp() = navController.navigateUp()
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        outState?.putParcelable(NavItem.PARCEL_KEY, currentNavItem)
+    override fun onSaveInstanceState(outState: Bundle) {
+        //outState.putParcelable(NavItem.PARCEL_KEY, currentNavItem)
         super.onSaveInstanceState(outState)
     }
 
-    private fun showFragment(navItem: NavItem) {
+    /*private fun showFragment(navItem: NavItem) {
         currentNavItem = navItem
         setTitle(navItem.screenTitleResId)
         navigationView.setCheckedItem(navItem.navItemId)
 
-        with(supportFragmentManager) {
+        *//*with(supportFragmentManager) {
             findFragmentByTag(navItem.fragmentTag).let { fragment ->
                 beginTransaction()
                     .replace(R.id.container, fragment ?: navItem.getNewFragment(), navItem.fragmentTag)
                     .apply { if (fragment == null) addToBackStack(null) }
                     .commitAllowingStateLoss()
             }
-        }
+        }*//*
     }
-
+*/
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
@@ -79,4 +91,13 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    class SaveDestination(navigatorProvider: NavigatorProvider): FragmentNavigator.Destination(navigatorProvider) {
+
+        override fun createFragment(args: Bundle?): Fragment {
+            return super.createFragment(args)
+        }
+    }
+
+
 }
